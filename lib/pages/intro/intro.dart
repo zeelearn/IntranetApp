51f2985cc5108948492efb3094d7361777d91b74
vishwazeelearn <141724793+vishwazeelearn@users.noreply.github.com>
2026-03-23 18:04:31 +0530

    feat: Enhance APIService and ZohoRequestModel for improved data handling and response parsing; update intro page layout for better user experience

--- lib/pages/intro/intro.dart
+++ lib/pages/intro/intro.dart
@@ -120,54 +120,61 @@ class _IntroPageState extends State<IntroPage> {
   Widget makePage({image, title, content, reverse = false}) {
     return Container(
       padding: EdgeInsets.only(left: 50, right: 50, bottom: 60),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          !reverse
-              ? Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Image.asset(image),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                  ],
-                )
-              : SizedBox(),
-          Text(
-            title,
-            style: TextStyle(
-                color: ColorSys.primary,
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
+      child: Center(
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 600),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                !reverse
+                    ? Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: Image.asset(image),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                        ],
+                      )
+                    : SizedBox(),
+                Text(
+                  title,
+                  style: TextStyle(
+                      color: ColorSys.primary,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  content,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: ColorSys.gray,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400),
+                ),
+                reverse
+                    ? Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: Image.asset(image),
+                          ),
+                        ],
+                      )
+                    : SizedBox(),
+              ],
+            ),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            content,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: ColorSys.gray,
-                fontSize: 20,
-                fontWeight: FontWeight.w400),
-          ),
-          reverse
-              ? Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Image.asset(image),
-                    ),
-                  ],
-                )
-              : SizedBox(),
-        ],
+        ),
       ),
     );
   }
